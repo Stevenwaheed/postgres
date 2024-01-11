526e96fdb915cb04d4135e0c24689538e8541f8c
Steven W. Saad <83607748+Stevenwaheed@users.noreply.github.com>
2024-01-11 16:37:55 +0200

    Update queries.go

--- queries.go
+++ queries.go
@@ -14,12 +14,12 @@ const newUserTransaction = `-- name: newUserTransaction :exec
     INSERT INTO users("name", "phone_number") VALUES ($1, $2)
 `
 
-type newUserTransactionParams struct {
+type NewUserTransactionParams struct {
 	Name        sql.NullString `json:"name"`
 	PhoneNumber string         `json:"phone_number"`
 }
 
-func (q *Queries) newUserTransaction(ctx context.Context, arg newUserTransactionParams) error {
+func (q *Queries) NewUserTransaction(ctx context.Context, arg newUserTransactionParams) error {
 	_, err := q.db.ExecContext(ctx, newUserTransaction, arg.Name, arg.PhoneNumber)
 	return err
 }
@@ -28,13 +28,13 @@ const otpTransaction = `-- name: otpTransaction :exec
     INSERT INTO users_otp("otp", "otp_expiration_time", "phone_number_otp") VALUES ($1, $2, $3)
 `
 
-type otpTransactionParams struct {
+type OtpTransactionParams struct {
 	Otp               sql.NullString `json:"otp"`
 	OtpExpirationTime sql.NullTime   `json:"otp_expiration_time"`
 	PhoneNumberOtp    string         `json:"phone_number_otp"`
 }
 
-func (q *Queries) otpTransaction(ctx context.Context, arg otpTransactionParams) error {
+func (q *Queries) VtpTransaction(ctx context.Context, arg otpTransactionParams) error {
 	_, err := q.db.ExecContext(ctx, otpTransaction, arg.Otp, arg.OtpExpirationTime, arg.PhoneNumberOtp)
 	return err
 }
@@ -43,17 +43,17 @@ const verifyOTP = `-- name: verifyOTP :one
     SELECT otp, otp_expiration_time FROM users_otp WHERE phone_number_otp=$1 AND otp=$2
 `
 
-type verifyOTPParams struct {
+type VerifyOTPParams struct {
 	PhoneNumberOtp string         `json:"phone_number_otp"`
 	Otp            sql.NullString `json:"otp"`
 }
 
-type verifyOTPRow struct {
+type VerifyOTPRow struct {
 	Otp               sql.NullString `json:"otp"`
 	OtpExpirationTime sql.NullTime   `json:"otp_expiration_time"`
 }
 
-func (q *Queries) verifyOTP(ctx context.Context, arg verifyOTPParams) (verifyOTPRow, error) {
+func (q *Queries) VerifyOTP(ctx context.Context, arg verifyOTPParams) (verifyOTPRow, error) {
 	row := q.db.QueryRowContext(ctx, verifyOTP, arg.PhoneNumberOtp, arg.Otp)
 	var i verifyOTPRow
 	err := row.Scan(&i.Otp, &i.OtpExpirationTime)
